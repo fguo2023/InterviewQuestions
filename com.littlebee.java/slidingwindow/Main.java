832489a307b0dfe977539db4b4cba8ad979b79a6
Leo <fguo2023>
2024-11-14 21:50:56 -0800

    Add the solution for leetcode 3. Longest Substring Without Repeating Characters

---
+++ com.littlebee.java/slidingwindow/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LongestNonRepeatedCharacter longestNonRepeatedCharacter = new LongestNonRepeatedCharacter();
+        longestNonRepeatedCharacter.lengthOfLongestSubstring("tmmzuxt");
+    }
+}
