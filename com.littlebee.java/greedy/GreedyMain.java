0e6df3c849c5ceb3ca3a4297746f195907937e79
Leo <fguo2023>
2024-11-17 22:58:00 -0800

    Added LexicographicalQuestion for greedy questions

---
+++ com.littlebee.java/greedy/GreedyMain.java
@@ -0,0 +1,8 @@
+package greedy;
+
+public class GreedyMain {
+    public static void main(String[] args) {
+        LexicographicalQuestion lexicographicalQuestion = new LexicographicalQuestion();
+        System.out.println(lexicographicalQuestion.findSmallest(new String[]{"b", "ba"}));
+    }
+}
